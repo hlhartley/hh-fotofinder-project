1ff518e9419619089f74ef1bc234c05ce1e3543b
Heather Hartley <hlhartley@gmail.com>
2018-11-02 16:21:30 -0600

    Add photo class

---
+++ photo.js
@@ -0,0 +1,21 @@
+class Photo {
+  constructor(id, title, caption) {
+    this.id = id;
+    this.title = title;
+    this.caption = caption;
+    this.file = file;
+    this.favorite = false;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updatePhoto() {
+
+  }
+}
